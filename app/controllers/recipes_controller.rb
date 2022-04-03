b1a55c53b3ef34d9ade05bd9d4c9b67373f88121
melanie rat <RAT.melanie@gmail.com>
2022-04-03 22:29:00 +0200

    search methode

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,19 +1,24 @@
 class RecipesController < ApplicationController
   def search
-    raise
-    @results = Recipe.where("data ->> ? ILIKE ?", search_params[:field], '%' + search_params[:value] + '%')
-    # Recipe.where("data ->> ? ILIKE ?", "ingredients", '%' + "flour" + '%') => 4.4ms
-    # Recipe.where("data ->> ? ILIKE ?", "ingredients", '%' + "flour" + '%' + "sugar" + '%')
-    # j'ai que à séparer mes ingredients de recherche par des %
-    # render @results.present? ? 'index' : 'no_results'
-    # ne pas oublier les strong params
-    # limit 5
+    # ma route ne correspond pas à mon controller
+    # je peux peut etre raccoursir
+    ingredients = search_params[:ingredients].lines.map(&:strip).join('%')
+    @matching_recipes = Recipe.where("data ->> ? ILIKE ?", 'ingredients', '%' + ingredients + '%')
     # est ce que ca va prendre celle qui corresponde le mieux ?
+    render :index
+  end
+
+  def index
+    
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
   end
 
 private
 
   def search_params
-    params.require(:search).permit :field, :value
+    params.require(:search).permit(:ingredients)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
-  # mais il me faut une route post
+
   resources :recipes, only: [:index, :show] do
     collection do 
       post :search
     end
-    # ca va pas ca me demander l'id d'une recette 
   end
 end
